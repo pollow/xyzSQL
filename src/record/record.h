70dd67ea4e49de5a8c50efd61ba03ee7a5ad77b5
pollow <pollow.xc@gmail.com>
2014-11-23 13:20:39 +0800

    Record api changed.

--- src/record/record.h
+++ src/record/record.h
@@ -4,18 +4,24 @@
 
 class Record {
     private:
-        vector<record_value> *values;
-        
+        vector<table_column *> *table_info;
+
+        void unpack();
+        void pack();
 
     public:
 		vector<unsigned char> buf;
-		Record() { };
-        Record( vector<record_value> *t ) : values(t) { }
-        Record( const vector<unsigned char> &tmp ) : buf(tmp) {  };
+        vector<record_value> values;
 
-        vector<record_value> unpack( vector<table_column *> *);
-        vector<unsigned char> pack( vector<table_column *> *);
-		
+		Record() { };
+
+        Record( vector<record_value> &t, vector<table_column *> *col ) : values(t), table_info(col) {
+            pack();
+        }
+        Record( const vector<unsigned char> &tmp, vector<table_column *> *col ) : table_info(col), buf(tmp) {
+            unpack();
+        };
+
 		inline uint32_t size() {
 			return buf.size();
 		}

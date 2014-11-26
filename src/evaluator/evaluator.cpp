90899896cce4a3559ff65fd40ce7075c68fbac40
pollow <pollow.xc@gmail.com>
2014-11-26 02:14:50 -0800

    wrong size of char array.

--- src/evaluator/evaluator.cpp
+++ src/evaluator/evaluator.cpp
@@ -31,7 +31,7 @@ bool verify_validation(vector<record_value> *r, vector<table_column *> *t) {
 
 string create_temp_table(vector<table_column *> *t) {
     uuid_t out;
-    char *uuid_str = new char[36];
+    char *uuid_str = new char[40];
     uuid_generate(out);
     uuid_unparse(out, uuid_str);
     create_table_stmt *cs = new create_table_stmt(uuid_str, t);
@@ -40,7 +40,7 @@ string create_temp_table(vector<table_column *> *t) {
     xyzsql_process_create_table(cs);
 
     string res(uuid_str);
-    delete uuid_str;
+    delete[] uuid_str;
 
     return res;
 }
